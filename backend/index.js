d97e74e055153582c62370b7fbb73c22cec6111b
Arthur Alves <arthur.alves.rs@gmail.com>
2021-12-09 14:25:57 -0300

    acesso ao path public

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 3000);
 let port = app.get('port');
